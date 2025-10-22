cd32a757f485e8f8ea726386f6d38533a168f77f
king99999999999999 <programmingfreak666@gmail.com>
2025-10-22 11:03:58 +0300

    Initial commit

---
+++ src/PrinterApp.java
@@ -0,0 +1,24 @@
+public class PrinterApp {
+    public static void main(String[] args) {
+        String creator = "Shaharear Ahmed Badhon";
+        double number1 = 5.0; 
+        double number2 = 2.0; 
+        double multiplication;
+        double addition;
+        double division;
+        double subtraction;
+        System.out.println("Printer App");
+        System.out.print("This app is made by: ");
+        System.out.println(creator);
+        System.out.println("The value of number1 is " + (int)number1);
+        System.out.println("The value of number2 is " + (int)number2);
+        multiplication = number1 * number2;
+        System.out.println((int)number1 + " * " + (int)number2 + " = " + (int)multiplication);
+        addition = number1 + number2;
+        System.out.println((int)number1 + " + " + (int)number2 + " = " + (int)addition);
+        division = number1 / number2;
+        System.out.println((int)number1 + " / " + (int)number2 + " = " + division);
+        subtraction = number1 - number2;
+        System.out.println((int)number1 + " - " + (int)number2 + " = " + (int)subtraction);
+    }
+}
